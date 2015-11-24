c555477b2e323e932841c48f3085ea559c9cb5e0
Mark Daniels <mark_daniels1982@hotmail.com>
2015-11-24 14:46:00 -0800

    install alchemy,postgres

--- ssitest.py
+++ ssitest.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, url_for, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def test():
-    return "WURKS, BOSS!"
+def home(name=None):
+    return render_template("home.html",name=name)
 
 if __name__ == '__main__':
      app.debug = True
